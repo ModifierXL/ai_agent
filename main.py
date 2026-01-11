5449e05b4bd5df56dd6b38986deaa416af32ff59
ModifierXL <benjamin.tresch@gmail.com>
2026-01-11 12:23:45 +0100

    refactoring

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ from google import genai
 from google.genai import types
 import argparse
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-if api_key is None:
-    raise RuntimeError("No API Key found!")
-
-client = genai.Client(api_key=api_key)
-ai_model = "gemini-2.5-flash"
 
 def main():
     parser = argparse.ArgumentParser(description="Chatbot")
@@ -20,14 +12,26 @@ def main():
     args = parser.parse_args()
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    result = client.models.generate_content(
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+
+    if not api_key:
+        raise RuntimeError("No API Key found!")
+
+    client = genai.Client(api_key=api_key)
+    ai_model = "gemini-2.5-flash"
+
+    response = client.models.generate_content(
         model = ai_model, 
         contents = messages)
     
+    if not response:
+        raise RuntimeError("Something went wrong with your request to the GEMINI API")
+    
     if args.verbose:
         print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {result.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {result.usage_metadata.candidates_token_count}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     
     print("Response:")
     print(result.text)
